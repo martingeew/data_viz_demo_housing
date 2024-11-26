96049e2ba405b118c1725bb51fe686acf0774ef9
martingeew <67925955+martingeew@users.noreply.github.com>
2024-11-26 19:08:43 +0100

    Update fonts

--- src/facet_vizualisation.py
+++ src/facet_vizualisation.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-from matplotlib.lines import Line2D # for the legend
+from matplotlib.lines import Line2D  # for the legend
 
-import matplotlib.patches as patches # for the legend
+import matplotlib.patches as patches  # for the legend
 from pyfonts import load_font
+from matplotlib.ticker import FuncFormatter
+import numpy as np
 
 
 ### Facet plot
@@ -24,10 +26,10 @@ data = pd.read_csv(
 
 ### Constants
 
-BLUE = '#2166ACFF'
-BLUE_LIGHT = '#4393C3FF'
-RED = '#B2182BFF'
-RED_LIGHT = '#D6604DFF'
+BLUE = "#2166ACFF"
+BLUE_LIGHT = "#4393C3FF"
+RED = "#B2182BFF"
+RED_LIGHT = "#D6604DFF"
 GREY40 = "#666666"
 GREY25 = "#404040"
 GREY20 = "#333333"
@@ -37,15 +39,16 @@ CHARCOAL = "#333333"
 
 # Load the fonts
 font = load_font(
-    "https://github.com/dharmatype/Bebas-Neue/blob/master/fonts/BebasNeue(2018)ByDhamraType/ttf/BebasNeue-Regular.ttf?raw=true"
+    "https://github.com/google/fonts/blob/main/ofl/cabincondensed/CabinCondensed-SemiBold.ttf?raw=true"
 )
 other_font = load_font(
-    "https://github.com/bBoxType/FiraSans/blob/master/Fira_Sans_4_3/Fonts/Fira_Sans_TTF_4301/Normal/Roman/FiraSans-Light.ttf?raw=true"
+    "https://github.com/google/fonts/blob/main/ofl/cabincondensed/CabinCondensed-Regular.ttf?raw=true"
 )
 other_bold_font = load_font(
-    "https://github.com/bBoxType/FiraSans/blob/master/Fira_Sans_4_3/Fonts/Fira_Sans_TTF_4301/Normal/Roman/FiraSans-Medium.ttf?raw=true"
+    "https://github.com/google/fonts/blob/main/ofl/cabincondensed/CabinCondensed-Medium.ttf?raw=true"
 )
 text_color = "black"
+
 
 def single_plot(x, y1, y2, name, ax):
 
@@ -77,14 +80,33 @@ def single_plot(x, y1, y2, name, ax):
     ax.yaxis.set_tick_params(which="both", length=0)
     ax.xaxis.set_tick_params(which="both", length=0)
 
+    # Custom function to convert y-tick values to 'k' format
+    def thousands_formatter(x, pos):
+        if x >= 1000:
+            return f"{int(x / 1000)}k"
+        else:
+            return f"{int(x)}"
+
+    # Customize the start, end, and frequency of the horizontal grid lines
+    y_start = 0.0
+    y_end = 70000
+    y_frequency = 10000
+    y_ticks = np.arange(y_start, y_end + y_frequency, y_frequency)
+    y_ticks = np.round(y_ticks).astype(int)
+    # Set the y-ticks
+    ax.set_yticks(y_ticks)
+
+    # Apply the custom formatter to y-axis
+    ax.yaxis.set_major_formatter(FuncFormatter(thousands_formatter))
+
     ax.spines["left"].set_color("none")
     ax.spines["bottom"].set_color("none")
     ax.spines["right"].set_color("none")
     ax.spines["top"].set_color("none")
     # added a 'size' argument
-    ax.set_title(name, weight="bold", size=9, color=CHARCOAL, font=other_bold_font)
-
-
+    ax.set_title(
+        name, weight="bold", size=12, color=CHARCOAL, font=other_bold_font, fontsize=12
+    )
 
 
 NROW = 3
@@ -123,7 +145,7 @@ for i, name in enumerate(NAMES):
 # Remove any unused subplots
 for j in range(len(NAMES), len(axes_flat)):
     fig.delaxes(axes_flat[j])
-    
+
 
 # Create handles for lines.
 handles = [
@@ -134,13 +156,13 @@ handles = [
 # Add legend for the lines
 fig.legend(
     handles=handles,
-    loc=(0.77, 0.92), # This coord is bottom-left corner
-    ncol=2,           # 1 row, 2 columns layout
+    loc=(0.75, 0.78),  # This coord is bottom-left corner
+    ncol=2,  # 1 row, 2 columns layout
     columnspacing=1,  # Space between columns
-    handlelength=1.2, # Line length
-    frameon=False,     # No frame
+    handlelength=1.2,  # Line length
+    frameon=False,  # No frame
     prop=other_font,
-    fontsize=14
+    fontsize=14,
 )
 
 
@@ -150,19 +172,19 @@ inflow = patches.Patch(facecolor=RED_LIGHT, alpha=0.3, label="Net inflow")
 
 fig.legend(
     handles=[outflow, inflow],
-    loc=(0.77, 0.89), # This coord is top-right corner
-    ncol=2,          # 1 row, 2 columns layout
-    columnspacing=1, # Space between columns
+    loc=(0.75, 0.75),  # This coord is top-right corner
+    ncol=2,  # 1 row, 2 columns layout
+    columnspacing=1,  # Space between columns
     handlelength=2,  # Area length
     handleheight=2,  # Area height
-    frameon=False,   # No frame
+    frameon=False,  # No frame
     prop=other_font,
-    fontsize=14
+    fontsize=14,
 )
 
 # Title
 fig.text(
-    s="Which migrants are replacing New Zealand citizens that leave?",
+    s="Which migrants are replacing the New Zealand citizens who leave?",
     x=0.05,
     y=1.025,
     color=CHARCOAL,
@@ -170,16 +192,16 @@ fig.text(
     font=font,
     ha="left",
     va="top",
-    fontweight="bold"
+    fontweight="bold",
 )
 
 # subtitle
 fig.text(
-    s="Long-term migration in New Zealand by citizenship\n(12-month rolling sum, top 9)",
+    s="Long-term migration in New Zealand by citizenship (12-month rolling sum, top 9 citizenships)",
     x=0.05,
     y=0.97,
     color=CHARCOAL,
-    fontsize=16,
+    fontsize=17,
     font=other_bold_font,
     ha="left",
     va="top",
@@ -199,6 +221,6 @@ fig.text(
 
 # Adjust layout and show the plot
 plt.tight_layout()
-plt.subplots_adjust(top=0.85, bottom=0.025)
+plt.subplots_adjust(top=0.85, bottom=0.04)
 plt.savefig("../reports/facet_migration", dpi=300, bbox_inches="tight")
 plt.show()
